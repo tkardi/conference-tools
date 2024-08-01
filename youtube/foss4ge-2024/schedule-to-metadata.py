1bc060424ea1743dd82b565d8f9d0393b682c19f
tkardi <tonis.kardi@gmail.com>
2024-08-01 22:06:29 +0300

    first initial working sample for FOSS4GE metadata conversion

--- youtube/foss4ge-2024/schedule-to-metadata.py
+++ youtube/foss4ge-2024/schedule-to-metadata.py
@@ -6,37 +6,42 @@
 import json, os, sys, unicodedata
 from collections import defaultdict
 from pathlib import PurePath
-from urllib import parse
+from urllib import parse, request
 import re
+import argparse
+
 
 import mistune
 # Make sure the `mdtoyt` can be found in the parent directory.
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from mdtoyt import YouTubeRenderer
 
-TITLE_PREFIX = 'FOSS4G 2022'
-CONF_HASHTAG = '#foss4g2022'
+TITLE_PREFIX = 'FOSS4GE 2024'
+CONF_HASHTAG = '#foss4ge2024'
+ACADEMICTRACK_NAME = "Academic track"
 # The keys match the conference acronym of the schedule.
 TYPE_HASHTAG = {
-    'foss4g-2022': '#generaltrack',
-    'foss4g-2022-academic-track': '#academictrack',
+    'foss4g-europe-2024': '#generaltrack',
 }
 # List of that talks that were not recorded or presented.
-TALKS_MISSING = ['GURC7K', 'BWTAEY', '79KBL9', 'GYAWLJ', 'WFLJKB', 'JAERFJ']
+TALKS_MISSING = [
+
+]
 # List of files that should be ignored.
 IGNORE_FILES = [
     # File with better audio is available.
-    '/osgeo/foss4gvideos/2022-08-26/Room_6/14 Sini P\u00f6yt\u00e4niemi - VIDEO ORIGINALE HA BUCHI DI AUDIO.mp4',
-    '/osgeo/foss4gvideos/2022-08-26/Room_6/14 nota.txt',
-    '/osgeo/foss4gvideos/2022-08-26/Room_9/5 non presente.txt',
-    '/osgeo/foss4gvideos/2022-08-24/Room_Hall_3A/6 no speaker.txt',
 ]
 # List of talks where the actual speaker isn't the one mentioned in pretalx
 ADDITIONAL_PERSONS = {
-    'SDG9K7': 'JulienOsman',
-    'JDGNJD': 'Anca Anghelea',
-    'XPCXBQ': 'Lorenzo Natali',
-    'XHUGFC': 'Antoine Drabble',
+
+}
+# Mapping between the room in the schedule.json and the uploaded files.
+ROOM_MAPPING = {
+    'Destination Earth (Van46 ring)': 'Destination_Earth',
+    'GEOCAT (301)': 'GEOCAT',
+    'LAStools (327)': 'LAStools',
+    'QFieldCloud (246)': 'QFieldCloud',
+    'Omicum': 'Omicum',
 }
 
 # From https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string/518232#518232
@@ -56,34 +61,25 @@ def ensure_https(url):
     return https.geturl()
 
 def to_hashtag(data):
-    '''Removes all whitespace and prepends a hash.'''
+    '''Removes all whitespace and prepends a hash.
+
+    Additionally turns the outbound hashtag to CamelCase
+    '''
     if data is None:
         return ''
     else:
-        return '#' + ''.join(data.split())
-
-
-# Mapping between the room in the schedule.json and the uploaded files.
-ROOM_MAPPING = {
-    'Auditorium': 'Auditorium',
-    'Room 4': 'Room_4',
-    'Modulo 0': 'Room_6',
-    'Room 9' : 'Room_9',
-    'Room Hall 3A': 'Room_Hall_3A',
-    'Room Limonaia': 'Room_Limonaia',
-    'Room Modulo 3': 'Room_Modulo_3A',
-    'Room Onice': 'Room_Onice',
-    'Room Verde': 'Room_Verde',
-}
+        return '#' + ''.join(x for x in data.title() if not x.isspace() and x.isalnum())
 
 def process_file_list(video_files_list):
     '''Convert a list of files into a nested dictionary.'''
-    result = defaultdict(lambda: defaultdict(list))
+    result = defaultdict(lambda: defaultdict(dict))
     with open(video_files_list) as video_files:
-        for video_file in video_files:
+        for row in video_files:
             # Extract the day and the room from the file path.
+            pretalx_link, video_file = row.split(';')
+            pretalx_id = PurePath(pretalx_link).parts[-1]
             day, room = PurePath(video_file).parts[-3:-1]
-            result[day][room].append(video_file.strip())
+            result[day][room][pretalx_id] = video_file.strip()
     return result
 
 def process_day(day, conf_prefix, videos):
@@ -98,29 +94,28 @@ def process_day(day, conf_prefix, videos):
         # found.
         talk_offset = 0
         for talk_counter, talk in enumerate(day['rooms'][room]):
-            talk_id = talk['url'].split('/')[5]
+            talk_id = talk['url'].split('/')[-2]
             slug = talk['slug'].removeprefix(f'{conf_prefix}-')
 
             # There are thing scheduled (like a group photo) which isn't a
             # talk. Those don't have a persons associated with it.
             # Make an exception for the OSGeo AGM (XMJZGY).
-            if not talk['persons'] and not talk_id == 'XMJZGY':
-                talk_offset += 1
+#            if not talk['persons'] and not talk_id == 'XMJZGY':
+#                talk_offset += 1
+#                continue
+
+            # @tkardi 1.08.2024: For FOSS4GE2024 we keep only track of the
+            # do_not_record == False
+            if talk['do_not_record'] == True:
                 continue
 
-
-            if talk_id in TALKS_MISSING:
-                talk_offset += 1
-                continue
+#            if talk_id in TALKS_MISSING:
+#                talk_offset += 1
+#                continue
 
             try:
-                while True:
-                    video_file = videos[date][room_name][talk_counter - talk_offset]
-                    if video_file in IGNORE_FILES:
-                        talk_offset -= 1
-                    else:
-                        break
-            except:
+                video_file = videos[date][room_name][talk_id]
+            except KeyError as ke:
                 video_file = 'ERROR: no video file found'
 
             title = f'{TITLE_PREFIX} | {talk["title"]}'
@@ -135,10 +130,20 @@ def process_day(day, conf_prefix, videos):
 
             pretalx_link = ensure_https(talk['url'])
 
-            hashtags_list = [CONF_HASHTAG, TYPE_HASHTAG[conf_prefix], to_hashtag(talk['track'])]
+            try:
+                if talk['track'] == ACADEMICTRACK_NAME:
+                    type_hashtag = ''
+                else:
+                    type_hashtag = TYPE_HASHTAG[conf_prefix]
+            except NameError:
+                pass
+
+            hashtags_list = [CONF_HASHTAG, type_hashtag, to_hashtag(talk['track'])]
             hashtags = '\\n'.join(hashtags_list)
 
-            description = f'{abstract}\\n\\n{persons}\\n\\n{pretalx_link}\\n\\n{hashtags}'
+
+
+            description = f'{abstract}\\n\\n{persons}\\n\\n{pretalx_link}\\n\\nRoom: {room} @ {talk["date"]}\\n\\n{hashtags}'
 
             metadata = {
                 'video_file': video_file,
@@ -151,36 +156,53 @@ def process_day(day, conf_prefix, videos):
             print(json.dumps(metadata))
 
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
+def main(**kwargs):
 
-    if len(argv) != 4:
-        print("Usage: {} schedule.json schedule_academic.json video-files.list".format(argv[0]))
-        sys.exit(1)
+    videos = process_file_list(kwargs['videofiles_list'])
+    print (videos)
+    for schedule_filename in kwargs['schedule']:
+        if not os.path.exists(schedule_filename):
+            assert schedule_filename.startswith('http://') or schedule_filename.startswith('https://'), \
+                f'Did not os.path find schedule {schedule_filename}, nor it is an URL'
+            with request.urlopen(schedule_filename) as r:
+                schedule_json = json.loads(r.read())
+            if kwargs['cache_schedule'] == True:
+                path = os.path.dirname(os.path.abspath(__file__))
+                p = PurePath(schedule_filename).parts[-2]
+                with open(os.path.join(path, f'{p}.json'), 'w') as fp:
+                    fp.write(json.dumps(schedule_json))
 
-    schedule_filename = argv[1]
-    schedule_academic_filename = argv[2]
-    video_files_list = argv[3]
+        else:
+            with open(schedule_filename, 'r') as schedule_file:
+                schedule_json = json.load(schedule_file)
 
-    videos = process_file_list(video_files_list)
-
-    with open(schedule_filename, 'r') as schedule_file:
-        schedule_json = json.load(schedule_file)
         conf_prefix = schedule_json['schedule']['conference']['acronym']
-        # First day of the talks is the third day of the conference
-        process_day(schedule_json['schedule']['conference']['days'][2], conf_prefix, videos)
-        process_day(schedule_json['schedule']['conference']['days'][3], conf_prefix, videos)
-        process_day(schedule_json['schedule']['conference']['days'][4], conf_prefix, videos)
+        schedule_days = schedule_json['schedule']['conference']['days']
+        for schedule_day in schedule_days:
+            process_day(schedule_day, conf_prefix, videos)
 
-    with open(schedule_academic_filename, 'r') as schedule_academic_file:
-        schedule_academic_json = json.load(schedule_academic_file)
-        conf_prefix = schedule_academic_json['schedule']['conference']['acronym']
-        # First day of the talks is the third day of the conference
-        process_day(schedule_academic_json['schedule']['conference']['days'][2], conf_prefix, videos)
-        process_day(schedule_academic_json['schedule']['conference']['days'][3], conf_prefix, videos)
-        process_day(schedule_academic_json['schedule']['conference']['days'][4], conf_prefix, videos)
         #print(conference_day1)
 
 if __name__ == '__main__':
-    sys.exit(main())
+    parser = argparse.ArgumentParser(prog=__file__)
+    parser.add_argument(
+        "-s", "--schedule",
+        help="Schedule filename. If many provide as many as needed separated by space.",
+        nargs="+",
+        required=True
+    )
+    parser.add_argument(
+        "-v", "--videofiles-list",
+        help="Path to preformatted list of videofiles.",
+        required=True
+    )
+    parser.add_argument(
+        "-c", "--cache-schedule",
+        help="Save the schedule file locally", action=argparse.BooleanOptionalAction,
+        default=False
+    )
+
+    args = parser.parse_args()
+    kwargs = args.__dict__
+
+    sys.exit(main(**kwargs))
